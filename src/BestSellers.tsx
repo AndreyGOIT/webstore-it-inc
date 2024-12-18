fb14ca97356b209b98e48d9c3bda1b9d1aefcaa6
AndreyGOIT <ae.borga@gmail.com>
2024-12-18 19:06:40 +0200

    add axios and state

--- src/BestSellers.tsx
+++ src/BestSellers.tsx
@@ -1,58 +1,80 @@
-export const BestSellers = () => {
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-    const products = [
-      {
-        _id: "664476983793ba7564c50ba2",
-        id: 8,
-        title: "Zip hoodie",
-        price: 91.99,
-        description: "Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.",
-        category: "men's clothing",
-        image: "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png",
-        rating: {
-          rate: 4.7,
-          count: 120
-        },
-        createdAt: "2024-05-15T08:47:20.180Z",
-        updatedAt: "2024-05-15T12:20:34.117Z",
-        __v: 0
-      },
-      {
-        _id: "664476983793ba7564c50ba2",
-        id: 9,
-        title: "Second Zip hoodie",
-        price: 191.99,
-        description: "Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.",
-        category: "men's clothing",
-        image: "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png",
-        rating: {
-          rate: 4.7,
-          count: 120
-        },
-        createdAt: "2024-05-15T08:47:20.180Z",
-        updatedAt: "2024-05-15T12:20:34.117Z",
-        __v: 0
-      },
-      {
-        _id: "664476983793ba7564c50ba2",
-        id: 10,
-        title: "Third Zip hoodie",
-        price: 291.99,
-        description: "Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.",
-        category: "men's clothing",
-        image: "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png",
-        rating: {
-          rate: 4.7,
-          count: 120
-        },
-        createdAt: "2024-05-15T08:47:20.180Z",
-        updatedAt: "2024-05-15T12:20:34.117Z",
-        __v: 0
-      }
-    ];
-  
-    return (
-    <div className="cards">
+export const BestSellers = () => {
+    const [products, setProducts] = useState([]);
+
+useEffect(() => {
+    //обращение к серверу при загрузке страницы
+    const promise = axios.get('https://masterclass.kimitsu.it-incubator.io/api/products')
+    promise.then((res) => {
+        const products = res.data;
+        setProducts(products);
+    })
+
+}, [])
+
+//   const products = [
+//     {
+//       _id: "664476983793ba7564c50ba2",
+//       id: 8,
+//       title: "Zip hoodie",
+//       price: 91.99,
+//       description:
+//         "Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.",
+//       category: "men's clothing",
+//       image:
+//         "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png",
+//       rating: {
+//         rate: 4.7,
+//         count: 120,
+//       },
+//       createdAt: "2024-05-15T08:47:20.180Z",
+//       updatedAt: "2024-05-15T12:20:34.117Z",
+//       __v: 0,
+//     },
+//     {
+//       _id: "664476983793ba7564c50ba2",
+//       id: 9,
+//       title: "Second Zip hoodie",
+//       price: 191.99,
+//       description:
+//         "Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.",
+//       category: "men's clothing",
+//       image:
+//         "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png",
+//       rating: {
+//         rate: 4.7,
+//         count: 120,
+//       },
+//       createdAt: "2024-05-15T08:47:20.180Z",
+//       updatedAt: "2024-05-15T12:20:34.117Z",
+//       __v: 0,
+//     },
+//     {
+//       _id: "664476983793ba7564c50ba2",
+//       id: 10,
+//       title: "Third Zip hoodie",
+//       price: 291.99,
+//       description:
+//         "Этот зип-худи, изготовленный из мягкого и уютного материала, предлагает идеальное сочетание стиля и комфорта для прохладной погоды. Его удобный крой с молнией спереди и капюшоном позволяет легко регулировать тепло, делая его идеальным выбором для повседневного ношения или для слоя одежды во время активных занятий на открытом воздухе.",
+//       category: "men's clothing",
+//       image:
+//         "https://production-it-incubator.s3.eu-central-1.amazonaws.com/file-manager/Image/e12cebe8-7d13-4842-98ab-f71e0df6a2f4_8-zip-hoodie.png",
+//       rating: {
+//         rate: 4.7,
+//         count: 120,
+//       },
+//       createdAt: "2024-05-15T08:47:20.180Z",
+//       updatedAt: "2024-05-15T12:20:34.117Z",
+//       __v: 0,
+//     },
+//   ];
+
+  return (
+    <div className="bestSeller">
+      <h2>Bestsellers</h2>
+      <div className="cards">
         {products.map((product) => (
           <div className="card" key={product._id}>
             <img src={product.image} alt="img" />
@@ -61,19 +83,7 @@ export const BestSellers = () => {
             <button>Show more</button>
           </div>
         ))}
-      {/* <div className="card">
-        <img src={products[0].image} alt="img" />
-        <h4>{products[0].title}</h4>
-        <p className="price">${products[0].price}</p>
-        <button>Show more</button>
       </div>
-      <div className="card">
-      <img src={products[0].image} alt="img" />
-      <h4>{products[0].title}</h4>
-      <p className="price">${products[0].price}</p>
-      <button>Show more</button>
-    </div> */}
     </div>
-    );
-  };
-  
+  );
+};
